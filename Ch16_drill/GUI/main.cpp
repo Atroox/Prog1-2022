528aa7bcfc42efabde44294c90fee2cc64d111a8
Tamási Ádám <adam.tmsi02@gmail.com>
2022-03-23 16:51:25 +0100

    Ch16 fel

---
+++ Ch16_drill/GUI/main.cpp
@@ -0,0 +1,22 @@
+/*
+    g++ main.cpp Graph.cpp Window.cpp GUI.cpp Lines_window.cpp -o main `fltk-config --ldflags --use-images`
+*/
+#include "Graph.h"
+#include "Lines_window.h"
+
+int main()
+try {
+	using namespace Graph_lib;
+
+	Lines_window win{Point{100, 100}, 600, 400, "lines"};
+
+	return gui_main();
+}
+catch(exception& e) {
+	cerr << "exception: " <<e.what() << endl;
+	return 1;
+}
+catch (...) {
+	cerr << "Some exception" << endl;
+	return 2;
+}
